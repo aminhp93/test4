8311445a6bf99f20fdc6f0caff921c86934fe9ef
Minh Pham <minhpham@Minhs-Mac-mini.localdomain>
2023-12-07 04:59:34 +0100

    Update

--- packages/remixapp/app/routes/index.tsx
+++ packages/remixapp/app/routes/index.tsx
@@ -1,5 +1,5 @@
-import { Header } from "@amin2/header";
-import { Footer } from "@amin2/footer";
+import { Header } from "amin93-header";
+import { Footer } from "amin93-footer";
 
 export default function Index() {
   return (
